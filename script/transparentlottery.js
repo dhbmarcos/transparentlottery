55b5b941fb0927fd69bc513f16963b62198ae589
dhbmarcos <dhbmarcos@gmail.com>
2026-05-11 21:53:05 -0300

    Draw process.

--- script/transparentlottery.js
+++ script/transparentlottery.js
@@ -1,6 +1,6 @@
 class TransparentLottery
 {
-    constructor(base, height, roll=0, terminal=undefined)
+    constructor(base, height=undefined, terminal=undefined)
     {
         /* Base */
         if (!Number.isInteger(base)) {
@@ -12,22 +12,15 @@ class TransparentLottery
         this._base = base;
 
         /* Height */
-        if (!Number.isInteger(height)) {
-            throw `Height paramenter must be an integer, not ${typeof height}.`;
-        }
-        if (height < 0) {
-            throw `Height paramenter must be greater than or equal to 0, not ${height}.`;
+        if (height !== undefined) {
+            if (!Number.isInteger(height)) {
+                throw `Height paramenter must be an integer, not ${typeof height}.`;
+            }
+            if (height < 0) {
+                throw `Height paramenter must be greater than or equal to 0, not ${height}.`;
+            }
         }
         this._height = height;
-
-        /* Roll */
-        if (!Number.isInteger(roll)) {
-            throw `Roll paramenter must be an integer, not ${typeof roll}.`;
-        }
-        if (roll < 0) {
-            throw `Roll paramenter must be greater than or equal to 0, not ${roll}.`;
-        }
-        this._roll = roll;
 
         /* Terminal */
         if (terminal !== undefined) {
@@ -36,20 +29,47 @@ class TransparentLottery
             }
         }
         this._terminal = terminal;
+
     }
 
-    async draw()
+    async draw(roll=0)
     {
-        const block = await this._getBlock();
+        /* Roll */
+        if (!Number.isInteger(roll)) {
+            throw `Roll paramenter must be an integer, not ${typeof roll}.`;
+        }
+        if (roll < 0) {
+            throw `Roll paramenter must be greater than or equal to 0, not ${roll}.`;
+        }
+
+        if (!this._height) {
+            await this._getCurrentHeight();
+        }
+
+        if (!this._seed) {
+            await this._getSeed()
+            this.logTerminal(`<b>Draw Rool Number 0</b> is the <b>Seed Draw Number</b>: <code>${this._seed}</code>.`);
+        }
+        let hash = this._seed;
+        hash = await this._rollHash(hash, roll);
+        let numbers = await this._getDrawNumbers(hash, this._base);
+        return numbers;
     }
 
-    async _getBlock()
+    async _getCurrentHeight()
+    {
+        const response = await fetch("https://mempool.space/api/blocks/tip/height");
+        this._height   = await response.json();
+        this.logTerminal(`Current height is <code>${this._height}</code>.`);
+    }
+
+    async _getSeed()
     {
         let response;
 
         try {
             const mempoolBlockHash = `https://mempool.space/api/block-height/${this._height}`;
-            this.logTerminal(`Getting <b>block</b> <code>${this._height}</code> from <b>Bitcoin Mempool</b>...`);
+            this.logTerminal(`Get the hash of block <code>${this._height}</code> from <b>Bitcoin Mempool</b> (<a href="${mempoolBlockHash}">${mempoolBlockHash}</a>)...`);
 
             try {
                 response = await fetch(mempoolBlockHash);
@@ -130,5 +150,61 @@ class TransparentLottery
 
         this._seed    = null;
         this._instant = date.toISOString();
+    }
+
+    async _rollHash(hash, roll=0)
+    {
+        if (roll > 0) {
+            this.logTerminal(`Applying the SHA256 hash function ${roll}x to get <b>Draw Rool Number</b>...`)
+        }
+
+        for (let currentRoll = 0; currentRoll < roll; currentRoll++) {
+            let bytes = new Uint8Array(hash.length / 2);
+            for (let position = 0; position < hash.length; position += 2) {
+                bytes[position / 2] = parseInt(hash.substr(position, 2), 16);
+            }
+            hash = await crypto.subtle.digest("SHA-256", bytes);
+            hash = Array.from(new Uint8Array(hash)).map(function (value) {
+                return value.toString(16).padStart(2, "0");
+            }).join("");
+            this.logTerminal(`<b>Draw Rool Number ${currentRoll + 1}</b>: <code>${hash}</code>.`);
+        }
+        return hash;
+    }
+
+    async _getDrawNumbers(hash, base)
+    {
+        try {
+            hash = hash.padStart(64, "0");
+            this.logTerminal(`Add zeros to the left of the Draw Rool Number to ensure it has 64 characters: <code">${hash}</code>`);
+
+            this.logTerminal("Convert the Draw Number to base <code>10</code> for further calculations using JavaScript:");
+            this.logTerminal(`<code>const draw_number_10 = BigInt("0x" + "${hash}")</code>`);
+            hash = BigInt("0x" + hash);
+            this.logTerminal(`<code>// draw_number_10 = "${hash}"</code>`);
+            this.logTerminal(`<code>const base_10 = BigInt("0x" + ${base})</code>`);
+            base = BigInt(base);
+            this.logTerminal(`<code>// base = "${hash}"</code>`);
+            this.logTerminal(`Convert the Draw Number to Game Base dividing it by <code>${base}</code> to get Draw Numbers. Each Draw Number is the rest the each division.`);
+
+            let numbers = [];
+            while (hash > 0) {
+                this.logTerminal(`<code>${hash} / ${base} = ${hash / base}<code> and rest <code>${hash % base}</code>. <code">${hash % base}</code> is a Draw Number.`);
+                if (!(hash / base > 0)) {
+                    this.logTerminal("The last result of division is zero, then stop division.");
+                }
+
+                numbers.push(Number(hash % base));
+                hash = hash / base;
+            }
+
+            let results = numbers.reverse();
+            this.logTerminal(`Revert sequence of results to get Draw Numbers.`);
+            this.logTerminal(`Draw Numbers: <code">${results}</code>`);
+            return results;
+        } catch (error) {
+            console.error(hash, error);
+            return `${hash} ${error}`;
+        }
     }
 }
