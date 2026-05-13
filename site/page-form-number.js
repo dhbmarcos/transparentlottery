96505797dcd01529e8d3ddddbf4d4f685b879c65
dhbmarcos <dhbmarcos@gmail.com>
2026-05-13 20:02:35 -0300

    Game page.

---
+++ site/page-form-number.js
@@ -0,0 +1,168 @@
+(function ()
+{
+    class PageFormNUmber extends HTMLElement
+    {
+        static get observedAttributes()
+        {
+            return ["name", "title", "description", "min", "max", "default", "disabled"];
+        }
+
+        constructor()
+        {
+            super();
+            this._shadowRoot = this.attachShadow({mode: "closed"});
+        }
+
+        attributeChangedCallback(attribute, oldValue, newValue)
+        {
+            if (oldValue === newValue) {
+                return;
+            }
+
+            this.render();
+        }
+
+        connectedCallback()
+        {
+            this.render();
+        }
+
+        get name()
+        {
+            return this.getAttribute("name") || "#";
+        }
+
+        set name(text)
+        {
+            this.setAttribute("name", text);
+        }
+
+        get title()
+        {
+            return this.getAttribute("title") || "#";
+        }
+
+        set title(text)
+        {
+            this.setAttribute("title", text);
+        }
+
+        get description()
+        {
+            return this.getAttribute("description") || "#";
+        }
+
+        set description(text)
+        {
+            this.setAttribute("description", text);
+        }
+
+        get min()
+        {
+            return this.getAttribute("min") || "#";
+        }
+
+        set min(number)
+        {
+            this.setAttribute("min", number);
+        }
+
+        get max()
+        {
+            return this.getAttribute("max") || "#";
+        }
+
+        set max(number)
+        {
+            this.setAttribute("max", number);
+        }
+
+        get default()
+        {
+            return this.getAttribute("default") || "#";
+        }
+
+        set default(number)
+        {
+            this.setAttribute("default", number);
+        }
+
+        get value()
+        {
+            const input = this._shadowRoot.querySelector("input");
+            if (!input) {
+                return this.getAttribute("default");
+            }
+            return input.value;
+        }
+
+        set value(number)
+        {
+            const input = this._shadowRoot.querySelector("input");
+            if (input) {
+                input.value = number;
+            }
+        }
+
+        get disabled()
+        {
+            return this.hasAttribute("disabled");
+        }
+
+        set disabled(value)
+        {
+            if (value) {
+                this.setAttribute("disabled", "");
+            } else {
+                this.removeAttribute("disabled");
+            }
+        }
+
+        render()
+        {
+            const name         = this.getAttribute("name");
+            const title        = this.getAttribute("title") || "";
+            const description  = this.getAttribute("description") || "";
+            const minValue     = this.getAttribute("min");
+            const maxValue     = this.getAttribute("max");
+            const defaultValue = this.getAttribute("default");
+            const isDisabled   = this.hasAttribute("disabled");
+
+            if (!name) {
+                throw `Invalid input name attribute: ${name}`;
+            }
+
+
+            let content =`
+                <link rel="stylesheet" href="https://www.w3schools.com/w3css/5/w3.css">
+                <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-deep-orange.css">
+                <link rel="stylesheet" href="/site/w3-webcomponents.css">
+
+                <label class="w3-margin-top">
+                    <b class="w3-text-theme">${title}</b>
+                    <br/>
+                    <span class="w3-small">${description}</span>
+                </label>
+                <input name="${name}" type="number" class="w3-input"
+            `
+
+            if (minValue) {
+                content += ` min="${minValue}"`;
+            }
+            if (maxValue) {
+                content += ` max="${maxValue}"`;
+            }
+            if (defaultValue) {
+                content += ` value="${defaultValue}"`;
+            }
+            if (isDisabled) {
+                content += ` disabled`;
+            }
+
+            content += ">";
+
+            this._shadowRoot.innerHTML = content;
+        }
+    }
+    customElements.define("page-form-number", PageFormNUmber);
+})();
---
+++ site/page-form.js
@@ -0,0 +1,83 @@
+(function ()
+{
+    class PageForm extends HTMLElement
+    {
+        static get observedAttributes()
+        {
+            return ["button-text", "disabled"];
+        }
+
+        constructor()
+        {
+            super();
+            this._shadowRoot = this.attachShadow({mode: "closed"});
+            this._shadowRoot.innerHTML = `
+                <link rel="stylesheet" href="https://www.w3schools.com/w3css/5/w3.css">
+                <link rel="stylesheet" href="https://www.w3schools.com/lib/w3-theme-deep-orange.css">
+                <link rel="stylesheet" href="/site/w3-webcomponents.css">
+
+                <slot></slot>
+                <button id="action-button" class="w3-button w3-theme w3-text-white w3-margin-top w3-block w3-round"></button>
+                `;
+        }
+
+        attributeChangedCallback(attribute, oldValue, newValue)
+        {
+            if (oldValue === newValue) {
+                return;
+            }
+
+            this.render();
+        }
+
+        connectedCallback()
+        {
+            this._shadowRoot.getElementById("action-button").addEventListener("click", () => {
+                this._getValues();
+            });
+            this.render();
+        }
+
+        _getValues()
+        {
+            const slot     = this._shadowRoot.querySelector('slot');
+            const elements = slot.assignedElements({ flatten: true });
+
+            const payload = {};
+
+            elements.forEach(function (element) {
+                if (element.tagName.toLowerCase() === 'page-form-number') {
+                    const key   = element.name;
+                    const value = element.value;
+
+                    if (key) {
+                        payload[key] = value;
+                    }
+                }
+            });
+
+            this.dispatchEvent(new CustomEvent('form-execute', {
+                detail:   payload,
+                bubbles:  true,
+                composed: true
+            }));
+        }
+
+        get button_text()
+        {
+            return this.getAttribute("button-text") || "#";
+        }
+
+        set button_text(text)
+        {
+            this.setAttribute("button-text", text);
+        }
+
+        render()
+        {
+            const button_text = this.getAttribute("button-text") || "Submit";
+            this._shadowRoot.getElementById("action-button").innerHTML = button_text;
+        }
+    }
+    customElements.define("page-form", PageForm);
+})();
